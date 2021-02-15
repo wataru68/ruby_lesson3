0dafb7b044ef3ca5f0b7470d9d72eef8b1387341
unknown <nishidawataru12@gmail.com>
2021-02-15 22:33:06 +0900

    first commit

---
+++ index.rb
@@ -0,0 +1,26 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+
+  def info
+    return "#{self.name} #{self.price}円"
+  end
+
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 3
+      total_price -= 100
+    end
+    return total_price
+  end
+end
+
+# 以下のコードは移動させないでください
+menu1 = Menu.new(name: "すし", price: 1000)
+
+puts menu1.info
